6f24aa5e73502e1d69e7f021bcade40ebcf13a74
MIDAV0 <magilnyy.vadimo@gmail.com>
2026-03-04 22:34:45 +0000

    Add server benches

---
+++ benches/server.rs
@@ -0,0 +1,164 @@
+use std::{net::SocketAddr, sync::{Arc, Barrier}, thread};
+
+use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
+use kvs::{KvClient, KvServer, KvStore, thread_pool::{SharedQueueThreadPool, ThreadPool}};
+use tempfile::TempDir;
+
+const NUM_REQUESTS: usize = 100;  // Reduced to stay within OS file descriptor limits
+const KEY_LENGTH: usize = 100;
+
+fn generate_keys(count: usize, key_len: usize) -> Vec<String> {
+    (0..count)
+        .map(|i| format!("{:0>width$}", i, width = key_len))
+        .collect()
+}
+
+fn write_queued_kvstore(c: &mut Criterion) {
+    let mut group = c.benchmark_group("write_queued_kvstore");
+    
+    let num_cpus = num_cpus::get();
+    let mut thread_counts: Vec<u32> = vec![1, 2, 4];
+    for i in (6..=(num_cpus * 2)).step_by(2) {
+        thread_counts.push(i as u32);
+    }
+    thread_counts.sort();
+    thread_counts.dedup();
+
+    let keys = generate_keys(NUM_REQUESTS, KEY_LENGTH);
+    let value = "benchmark_value".to_string();
+    
+    let mut base_port = 5000;
+    
+    for server_threads in thread_counts {
+        let temp_dir = TempDir::new().unwrap();
+        let engine = KvStore::open(temp_dir.path()).unwrap();
+        let server_pool = SharedQueueThreadPool::new(server_threads).unwrap();
+        let server = KvServer::new(engine, server_pool);
+        
+        let port = base_port;
+        base_port += 1;
+        let server_addr: SocketAddr = format!("127.0.0.1:{}", port).parse().unwrap();
+        
+        thread::spawn(move || {
+            let _ = server.start(server_addr);
+        });
+        
+        thread::sleep(std::time::Duration::from_millis(200));
+        
+        let client_pool = SharedQueueThreadPool::new(NUM_REQUESTS as u32).unwrap();
+        let keys = Arc::new(keys.clone());
+        let value = Arc::new(value.clone());
+        let server_addr_str = Arc::new(format!("127.0.0.1:{}", port));
+        
+        group.bench_with_input(
+            BenchmarkId::new("threads", server_threads),
+            &server_threads,
+            |b, _| {
+                b.iter(|| {
+                    // Create fresh barrier for each iteration
+                    let barrier = Arc::new(Barrier::new(NUM_REQUESTS + 1));
+                    
+                    for i in 0..NUM_REQUESTS {
+                        let key = keys[i].clone();
+                        let value = (*value).clone();
+                        let addr = (*server_addr_str).clone();
+                        let barrier = Arc::clone(&barrier);
+                        
+                        client_pool.spawn(move || {
+                            // Create fresh connection for each request
+                            // (server closes connection after handling request)
+                            let mut client = KvClient::connect(addr).unwrap();
+                            let result = client.set(key, value);
+                            assert!(result.is_ok(), "Set failed: {:?}", result);
+                            barrier.wait();
+                        });
+                    }
+                    
+                    barrier.wait();
+                });
+            }
+        );
+    }
+    
+    group.finish();
+}
+
+
+
+fn read_queued_kvstore(c: &mut Criterion) {
+    let mut group = c.benchmark_group("read_queued_kvstore");
+    
+    let num_cpus = num_cpus::get();
+    let mut thread_counts: Vec<u32> = vec![1, 2, 4];
+    for i in (6..=(num_cpus * 2)).step_by(2) {
+        thread_counts.push(i as u32);
+    }
+    thread_counts.sort();
+    thread_counts.dedup();
+
+    let keys = generate_keys(NUM_REQUESTS, KEY_LENGTH);
+    let value = "benchmark_value".to_string();
+    
+    let mut base_port = 5000;
+    
+    for server_threads in thread_counts {
+        let temp_dir = TempDir::new().unwrap();
+        let engine = KvStore::open(temp_dir.path()).unwrap();
+        let server_pool = SharedQueueThreadPool::new(server_threads).unwrap();
+        let server = KvServer::new(engine, server_pool);
+        
+        let port = base_port;
+        base_port += 1;
+        let server_addr: SocketAddr = format!("127.0.0.1:{}", port).parse().unwrap();
+        
+        thread::spawn(move || {
+            let _ = server.start(server_addr);
+        });
+        
+        thread::sleep(std::time::Duration::from_millis(200));
+
+        for key in keys.iter() {
+            let mut client = KvClient::connect(server_addr.to_string()).unwrap();
+            client.set(key.clone(), value.clone()).unwrap();
+        }
+        
+        let client_pool = SharedQueueThreadPool::new(NUM_REQUESTS as u32).unwrap();
+        let keys = Arc::new(keys.clone());
+        let server_addr_str = Arc::new(format!("127.0.0.1:{}", port));
+        
+        group.bench_with_input(
+            BenchmarkId::new("threads", server_threads),
+            &server_threads,
+            |b, _| {
+                b.iter(|| {
+                    // Create fresh barrier for each iteration
+                    let barrier = Arc::new(Barrier::new(NUM_REQUESTS + 1));
+                    
+                    for i in 0..NUM_REQUESTS {
+                        let key = keys[i].clone();
+                        let addr = (*server_addr_str).clone();
+                        let barrier = Arc::clone(&barrier);
+                        
+                        client_pool.spawn(move || {
+                            // Create fresh connection for each request
+                            // (server closes connection after handling request)
+                            let mut client = KvClient::connect(addr).unwrap();
+                            let result = client.get(key);
+                            assert!(result.is_ok(), "Set failed: {:?}", result);
+                            barrier.wait();
+                        });
+                    }
+                    
+                    barrier.wait();
+                });
+            }
+        );
+    }
+    
+    group.finish();
+}
+
+
+
+criterion_group!(benches, write_queued_kvstore, read_queued_kvstore);
+criterion_main!(benches);
--- src/server.rs
+++ src/server.rs
@@ -50,44 +50,32 @@ impl<E: KvsEngine, P: ThreadPool> KvServer<E, P> {
 fn handle_stream<E: KvsEngine>(mut stream: TcpStream, engine: E) -> Result<()> {
     let peer_addr = stream.peer_addr()?;
 
-    loop {            
-        let req: Request = match recv_message(&mut stream) {
-            Ok(req) => req,
-            Err(e) if matches!(e, KvsError::EOF) => {
-                slog::info!(slog_scope::logger(), "Client disconnected"; "client_addr" => peer_addr);
-                break;
-            },
-            Err(e) => {
-                slog::error!(slog_scope::logger(), "Failed to receive message"; "err" => format!("{:?}", e), "from" => peer_addr);
-                break;
-            }
-        };
+    let req: Request = recv_message(&mut stream)?;
 
-        slog::info!(slog_scope::logger(), "Received request"; "req" => format!("{:?}", req), "from" => peer_addr);
+    slog::info!(slog_scope::logger(), "Received request"; "req" => format!("{:?}", req), "from" => peer_addr);
 
-        macro_rules! send_resp {
-            ($resp:expr) => {{
-                let resp = $resp;
-                send_message(&mut stream, &resp)?;
-                slog::debug!(slog_scope::logger(), "Response sent"; "resp" => format!("{:?}", resp), "to" => peer_addr);
-            }};
-        }
-
-        match req {
-            Request::Get(k) => send_resp!(match engine.get(k) {
-                Ok(item) => GetResponse::Ok(item),
-                Err(e) => GetResponse::Err(format!("{}", e)),
-            }),
-            Request::Set(k, v) => send_resp!(match engine.set(k, v) {
-                Ok(_) => SetResponse::Ok(()),
-                Err(e) => SetResponse::Err(format!("{}", e)),
-            }),
-            Request::Rm(k) => send_resp!(match engine.remove(k) {
-                Ok(_) => RemoveResponse::Ok(()),
-                Err(e) => RemoveResponse::Err(format!("{}", e)),
-            }),
-        };
+    macro_rules! send_resp {
+        ($resp:expr) => {{
+            let resp = $resp;
+            send_message(&mut stream, &resp)?;
+            slog::debug!(slog_scope::logger(), "Response sent"; "resp" => format!("{:?}", resp), "to" => peer_addr);
+        }};
     }
+
+    match req {
+        Request::Get(k) => send_resp!(match engine.get(k) {
+            Ok(item) => GetResponse::Ok(item),
+            Err(e) => GetResponse::Err(format!("{}", e)),
+        }),
+        Request::Set(k, v) => send_resp!(match engine.set(k, v) {
+            Ok(_) => SetResponse::Ok(()),
+            Err(e) => SetResponse::Err(format!("{}", e)),
+        }),
+        Request::Rm(k) => send_resp!(match engine.remove(k) {
+            Ok(_) => RemoveResponse::Ok(()),
+            Err(e) => RemoveResponse::Err(format!("{}", e)),
+        }),
+    };
 
     Ok(())
 }
