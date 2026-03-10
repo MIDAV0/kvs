c9eddcff0d79e950357764f506342abae649a65b
MIDAV0 <magilnyy.vadimo@gmail.com>
2026-03-10 11:47:50 +0000

    Update to thiserror implementation

--- src/error.rs
+++ src/error.rs
@@ -1,64 +1,37 @@
-use failure::Fail;
 use std::io;
 use bincode;
 use serde_json;
+use thiserror::Error;
 
 pub type Result<T> = std::result::Result<T, KvsError>;
 
-#[derive(Debug, Fail)]
+#[derive(Debug, Error)]
 pub enum KvsError {
-    #[fail(display = "invalid io: {}", _0)]
-    Io(#[cause] io::Error),
-    #[fail(display = "failed io write to file: {}", _0)]
-    Serde(#[cause] serde_json::Error),
-    #[fail(display = "Key not found")]
+    #[error("invalid io: {}", _0)]
+    Io(#[from] io::Error),
+    #[error("failed io write to file: {}", _0)]
+    Serde(#[from] serde_json::Error),
+    #[error("Key not found")]
     KeyNotFound,
-    #[fail(display = "unexpected command")]
+    #[error("unexpected command")]
     UnexpectedCommandType,
-    #[fail(display = "couldnt find reader")]
+    #[error("couldnt find reader")]
     ReaderNotFound,
-    #[fail(display = "invalid engine")]
+    #[error("invalid engine")]
     InvalidEngine,
     /// Bincode error
-    #[fail(display = "bincode error: {}", _0)]
-    Bincode(#[cause] bincode::Error),
+    #[error("bincode error: {}", _0)]
+    Bincode(#[from] bincode::Error),
     /// Sled error
-    #[fail(display = "sled error: {}", _0)]
-    Sled(#[cause] sled::Error),
+    #[error("sled error: {}", _0)]
+    Sled(#[from] sled::Error),
     /// Error with a string message
-    #[fail(display = "{}", _0)]
+    #[error("{}", _0)]
     StringError(String),
     /// Eof
-    #[fail(display = "EOF")]
+    #[error("EOF")]
     EOF,
     /// ThreadPool
-    #[fail(display = "threadpool error: {}", _0)]
+    #[error("threadpool error: {}", _0)]
     ThreadPool(String),
-}
-
-impl From<io::Error> for KvsError {
-    fn from(err: io::Error) -> KvsError {
-        match err.kind() {
-            io::ErrorKind::UnexpectedEof => KvsError::EOF,
-            _ => KvsError::Io(err),
-        }
-    }
-}
-
-impl From<serde_json::Error> for KvsError {
-    fn from(err: serde_json::Error) -> KvsError {
-        KvsError::Serde(err)
-    }
-}
-
-impl From<sled::Error> for KvsError {
-    fn from(err: sled::Error) -> KvsError {
-        KvsError::Sled(err)
-    }
-}
-
-impl From<bincode::Error> for KvsError {
-    fn from(err: bincode::Error) -> KvsError {
-        KvsError::Bincode(err)
-    }
 }
